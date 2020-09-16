a1ca048ce833e522c895827aa3437e4ca2f37c32
Enzo Haguiwara <haguiwara68@gmail.com>
2020-09-16 20:14:07 -0300

    Update schema with offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_16_020142) do
+ActiveRecord::Schema.define(version: 2020_09_16_230846) do
 
   create_table "campuses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
@@ -34,6 +34,17 @@ ActiveRecord::Schema.define(version: 2020_09_16_020142) do
     t.index ["university_id"], name: "index_courses_on_university_id"
   end
 
+  create_table "offers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.float "full_price"
+    t.float "discount_percentage"
+    t.date "start_date"
+    t.string "enrollment_semester"
+    t.bigint "course_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_id"], name: "index_offers_on_course_id"
+  end
+
   create_table "universities", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.float "score"
@@ -45,4 +56,5 @@ ActiveRecord::Schema.define(version: 2020_09_16_020142) do
   add_foreign_key "campuses", "universities"
   add_foreign_key "courses", "campuses"
   add_foreign_key "courses", "universities"
+  add_foreign_key "offers", "courses"
 end
