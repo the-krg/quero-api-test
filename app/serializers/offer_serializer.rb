9eb8940773c85e56be6fdf8c524964347e723812
Enzo Haguiwara <haguiwara68@gmail.com>
2020-09-24 16:07:31 -0300

    Add price with discount to offer serializer

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,7 +1,11 @@
 class OfferSerializer < ActiveModel::Serializer
-  attributes :full_price, :discount_percentage, :start_date, :enrollment_semester, :enabled
+  attributes :full_price, :discount_percentage, :start_date, :enrollment_semester, :enabled, :price_with_discount
 
   belongs_to :course
   belongs_to :university
   belongs_to :campus
+
+  def price_with_discount
+    object.full_price - ((object.full_price * object.discount_percentage)/100).truncate(2)
+  end
 end
